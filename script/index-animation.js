e8ffdf9f9f34d3815e5b53b231cc8ff8eebc38dd
Lukasz <lucas.balcerak@gmail.com>
2023-03-30 23:57:38 +0200

    Git init + offline work
    index.html skeleton done. With first look of the page.Some animations

---
+++ script/index-animation.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  var header = document.getElementById('header');
+  header.style.setProperty('right', '0');
+});
