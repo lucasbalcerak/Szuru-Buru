512bc297faed9ed37ec8118c89baf7166e7737ea
Lukasz <lucas.balcerak@gmail.com>
2023-03-31 23:45:24 +0200

    Load animation deleted

--- script/index-animation.js
+++ script/index-animation.js
@@ -1,5 +1,1 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM fully loaded and parsed');
-  var header = document.getElementById('header');
-  header.style.setProperty('right', '0');
-});
+
