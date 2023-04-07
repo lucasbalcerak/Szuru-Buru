dd453d90e16cd3de0f47dc8b524acab9c2def6b9
Lukasz <lucas.balcerak@gmail.com>
2023-04-07 22:59:08 +0200

    logic for empty input done

--- script/contact-us-script.js
+++ script/contact-us-script.js
@@ -1,6 +1,30 @@
-$(document).ready(function() {    
+$(document).ready(function() {  
+
     $('#send').on('click', function(){
-        if($('.modal').hasClass('none')){
+        var nameValue = $('#name').val();
+        var emailValue = $('#email').val();
+        var textValue = $('#text').val();
+
+        console.log(nameValue != "");
+        console.log(emailValue);
+        console.log(textValue);
+
+        if(nameValue === "" && $('#name-req').hasClass('none')) {
+            $('#name-req').removeClass('none');
+        } else {
+            $('#name-req').addClass('none');
+        }
+
+        if(emailValue === "" && $('#email-req').hasClass('none')) {
+            $('#email-req').removeClass('none');
+        } else {
+            $('#email-req').addClass('none');
+        }
+
+        if($('.modal').hasClass('none') && 
+        nameValue != "" &&
+        emailValue != "" &&
+        textValue != "" ){
             $('.modal').removeClass('none');
             $('.site-cover').removeClass('none');
         }        
