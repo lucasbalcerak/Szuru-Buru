770d48d2faa7db5afdcd0102433fae8cd192e19b
Lukasz <lucas.balcerak@gmail.com>
2023-04-10 22:00:10 +0200

    Better validation to form done, name to message added

--- script/contact-us-script.js
+++ script/contact-us-script.js
@@ -1,34 +1,39 @@
-$(document).ready(function() {  
+$(document).ready(function(e) {  
 
-    $('#send').on('click', function(){
+    $('#send').on('click', function(e){
+        e.preventDefault();
+
         var nameValue = $('#name').val();
         var emailValue = $('#email').val();
-        var textValue = $('#text').val();
+        var nameBool = false;
+        var emailBool = false;
 
         var pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-        console.log(pattern.test(emailValue));
-
-        if(nameValue === "" && $('#name-req').hasClass('none')) {
+        console.log($('#thanks-h2').html());
+        if(nameValue === "") {
             $('#name-req').removeClass('none');
+            nameBool = false;
         } else {
             $('#name-req').addClass('none');
+            nameBool = true;
         }
 
         if(!pattern.test(emailValue)) {
             $('#email-req').removeClass('none');
+            emailBool = false;
         } else {
             $('#email-req').addClass('none');
+            emailBool = true;
         }
 
         if($('.modal').hasClass('none') && 
-        nameValue != "" &&
-        pattern.test(emailValue) &&
-        textValue != "" ){
-            console.log("Cps");
+        nameBool &&
+        emailBool ){
             $('.modal').removeClass('none');
             $('.site-cover').removeClass('none');
-        }        
+            $('#thanks-h2').html("Dziękujemy za wiadomość " + nameValue);
+        }  
     })
 
     $('#close').on('click', function(){
@@ -38,3 +43,5 @@ $(document).ready(function() {
         }        
     })
 });
+
+
